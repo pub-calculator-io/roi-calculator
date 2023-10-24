8483ad1f4acde012429c30a39bf1a91004c2254a
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:11:36 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_roi_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Return on Investment (ROI) Calculator by Calculator.iO";
 
 function display_ci_roi_calculator(){
